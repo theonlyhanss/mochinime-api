64438582c4d88edceed4a0ed5b1c761a156a5f29
HANMETAFORCE <xyzhansnew850@gmail.com>
2025-08-07 20:59:45 +0700

    chore: move server

--- src/swagger.js
+++ src/swagger.js
@@ -9,13 +9,13 @@ const options = {
       description: 'Dokumentasi REST API Mochinime untuk streaming anime subtitle Indonesia.\n\n[Website](https://hanmetaforce.my.id)\n\n[Donate here](https://saweria.co/hanmetaforce)'
     },
     servers: [
-      {
-        url: 'http://localhost:3000',
-        description: 'Local server'
-      },
-      {
+     {
         url: 'https://mochinime-api.vercel.app',
         description: 'Production server'
+     },
+     {
+        url: 'http://localhost:3000',
+        description: 'Local server'
       }
     ]
   },
@@ -25,3 +25,4 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options);
 
 export default swaggerSpec;
+
